aee18c6f63f8d1373fc708347dd0505e92a54894
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2022-12-17 23:03:58 +0700

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 2, 3, 100, 324, 343, 44, 6, 33, 100};
+
+int n = array.Length;
+int find = 100;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index += 1;
+}
