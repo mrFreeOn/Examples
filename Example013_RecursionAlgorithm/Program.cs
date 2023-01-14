d5ce64cc2224b70d5e92ddb92d87c4e91cb3cae3
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-14 20:28:13 +0700

    add_example013_finish

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/* 
 string[,] table = new string[2,5];
 
 
@@ -10,4 +10,81 @@ for (int rows = 0; rows< 5; rows++)
     {
         Console.WriteLine($"-{table[rows, columns]}-");
     }
+} */
+
+int[,] matrix = new int[3, 4];
+int[,] pic = new int[,]
+{
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0,0,0},
+    {0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0},
+    {0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0},
+    {0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,1,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+};
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0) ; i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
 }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0) ; i++)
+    {
+        for (int j = 0; j < matr.GetLength(0) ; j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); //[1; 10)
+         }
+    }
+}
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0) ; i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            /* Console.Write($"{matr[i, j]} "); */
+            if(image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    }
+}
+
+
+/* PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix); */
+PrintImage(pic);
+FillImage(7, 10);
+PrintImage(pic);
